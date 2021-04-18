28b8149e9c810210026daff05f06961d4fcc8fd6
trantanhau <trantanhau281015@gmail.com>
2021-04-18 10:48:19 +0700

    add login

---
+++ RolexStore/RolexStore/Common/CommonConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RolexStore.Common
+{
+    public static class CommonConstants
+    {
+        public static string USER_SESSION = "USER_SESSION";
+    }
+}
---
+++ RolexStore/RolexStore/Common/UserLogin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RolexStore.Common
+{
+    [Serializable]
+    public class UserLogin
+    {
+        public int UserID { get; set; }
+        public string Email { get; set; }
+    }
+}
